82acfa7d1f0bf6a3cd85a24d1f15d704bdb23d53
Billy Fu <billyfu@osgeek.net>
2026-03-27 08:39:04 +0800

    rollback

---
+++ config.js
@@ -0,0 +1,26 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Uptime-Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2702932-1b2d0445c285fc26514ee217',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '返回主页',
+      url: 'https://www.sysgeek.cn/',
+      className: 'home-button'
+    },
+  ],
+};
